aa442d30b801729f17ca4f8aa79700deace72296
Christian Glahn <glah@zhaw.ch>
2023-05-04 14:04:48 +0200

    initial version and production dependencies

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # yaml-configurator
 Read YAML configs from well-known locations
+
+## Motivation
+
+JS micro-services require a configuration. As this is such a common problem this package unifies this tasks by providing a standard interface for loading configurations from well-known locations. 
+
+## Getting started
+
+```bash
+npm i @phish108/yaml-configurator
+```
+
+In the index module: 
+
+```js
+import * as Config from "./ConfigReader.mjs";
+
+// load from the first location that matches.
+const cfg = await Config.readConfig(
+        [
+            "/etc/app/config.yaml",
+            "./config.yaml", 
+            "./tools/config.yaml"
+        ]
+    );
+```
+
+## Usage
+
+TBD
---
+++ src/index.mjs
@@ -0,0 +1,57 @@
+import YAML from "yaml";
+import fs from "fs/promises";
+// const fs = require("node:fs/promises");
+
+export async function readConfig(locations, keys = [], defaults = {}) {
+    let result = {};
+
+    if (!(keys && Array.isArray(keys))) {
+        keys = [];
+    }
+
+    if ( typeof locations === "string" ) {
+        locations = [locations];
+    }
+
+    const locs = await Promise.all(
+        locations.map((afile) => fs.stat(afile)
+            .then(() => afile)
+            .catch(() => undefined))
+    );
+
+    const file = locs.filter((e) => e !== undefined).shift();
+
+    if (file === undefined) {
+        console.log(`${JSON.stringify({module: __filename, message: "No config files, return defaults"})}`);
+        return defaults;
+    }
+
+    try {
+        const cfgdata = await fs.readFile(file, "utf-8");
+
+        if (cfgdata !== undefined) {
+            result = YAML.parse(cfgdata);
+        }
+    }
+    catch (err) {
+        console.log(`${JSON.stringify({
+            module: __filename,
+            message: "cannot read file",
+            extra: err.message})}`);
+    }
+
+    if (!result) {
+        console.log(`${JSON.stringify({module: __filename, message: "Empty config, return defaults"})}`);
+        result = defaults;
+    }
+
+    result = keys.reduce((acc, k) => {
+        if (!(k in acc)) {
+            acc[k] = defaults[k];
+        }
+        
+        return acc; 
+    }, result)
+
+    return result;
+}
