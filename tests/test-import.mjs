6cc8936f088094a230da01aa0c6c71972c0314cc
Christian Glahn <glah@zhaw.ch>
2023-05-07 22:18:45 +0200

    better test decoration

--- tests/test-import.mjs
+++ tests/test-import.mjs
@@ -2,9 +2,13 @@ import {describe, it} from "node:test";
 import assert from 'node:assert/strict';
 import {readConfig} from "../src/index.mjs";  // <--- this is the import that is being tested
 
-describe("Test config reader with nested key checks", async () => {
+describe("Test config reader with imports", async () => {
 
     it("import from path absolute", async () => {
-        
+
+    });
+
+    it("import from path relative", async () => {
+
     });
 });
