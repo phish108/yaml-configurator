b6d790e6064326e98443b6c1d0d5682f62eb3ea7
Christian Glahn <glah@zhaw.ch>
2023-05-07 22:16:20 +0200

    test stubs

---
+++ tests/test-import.mjs
@@ -0,0 +1,10 @@
+import {describe, it} from "node:test";
+import assert from 'node:assert/strict';
+import {readConfig} from "../src/index.mjs";  // <--- this is the import that is being tested
+
+describe("Test config reader with nested key checks", async () => {
+
+    it("import from path absolute", async () => {
+        
+    });
+});
